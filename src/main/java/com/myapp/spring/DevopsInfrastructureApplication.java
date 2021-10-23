c46f2cd99ab4db0da3010f39c3d60fc0a2e91aee
Ramanji <d.ramanjireddy521@gmail.com>
2021-10-23 13:20:35 +0530

    ci cd

---
+++ src/main/java/com/myapp/spring/DevopsInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsInfrastructureApplication.class, args);
+	}
+
+}
